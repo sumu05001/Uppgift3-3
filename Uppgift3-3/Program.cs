a5a4d6da3d20cd9bff5dca20ac80dc8a3ae99a3d
Tsnem Musa <tm222qf@student.lnu.se>
2023-02-17 16:50:27 +0100

    Add project files.

---
+++ Uppgift3-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int avgift = 80;
+            int maximala = 950;
+            Console.WriteLine("hur mång timmar vill du hyra bilen ");
+            int timmar = Convert.ToInt32(Console.ReadLine());
+            int kosta = avgift * timmar;
+            if (kosta > maximala)
+            {
+                kosta = 950;
+            }
+            Console.WriteLine($"Kostnaden för att hyra bilen i {timmar} timmar är {kosta} kr.");
+
+        }
+    }
+}
